ce05787e356099b591f2f666d480fc4c2758ce89
Tim Golden <mail@timgolden.me.uk>
2012-03-13 13:22:54 +0000

    Very slightly annotate the test runner and up the possible versions to 3.3 (even though I don't have an installed version running here)

--- test.cmd
+++ test.cmd
@@ -1,3 +1,6 @@
+REM
+REM Run tests for all available versions of Python between 2.4 & 3.3
+REM
 @echo off
-for /l %%n in (24,1,32) do if exist c:\python%%n\python.exe (echo. & echo python%%n & c:\python%%n\python.exe -W ignore test_winshell.py)
+for /l %%n in (24,1,33) do if exist c:\python%%n\python.exe (echo. & echo python%%n & c:\python%%n\python.exe -W ignore test_winshell.py)
 pause
