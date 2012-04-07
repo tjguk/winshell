179fc36c32b1d6bd57d8577a1b78305daf2951b2
Tim Golden <mail@timgolden.me.uk>
2012-04-07 12:25:21 +0100

    Move PEP8 check inside test script

--- test.cmd
+++ test.cmd
@@ -2,5 +2,6 @@ REM
 REM Run tests for all available versions of Python between 2.4 & 3.3
 REM
 @echo off
+python -mpep8 --max-line-length=120 --ignore=E401,E501,W292,E302,E261 .
 for /l %%n in (24,1,33) do if exist c:\python%%n\python.exe (echo. & echo python%%n & c:\python%%n\python.exe -W ignore test_winshell.py)
 pause
